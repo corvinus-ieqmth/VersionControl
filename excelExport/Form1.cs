e3bb5fe2c15609f7fff8d2aa8295d0a2684a0739
Tothadam80 <toth.adam80@gmail.com>
2021-10-08 19:10:10 +0200

    adatok betöltése

--- excelExport/Form1.cs
+++ excelExport/Form1.cs
@@ -12,9 +12,23 @@ namespace excelExport
 {
     public partial class Form1 : Form
     {
+        RealEstateEntities context = new RealEstateEntities();
+        List<Flat> flats;
+
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            flats = context.Flats.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
